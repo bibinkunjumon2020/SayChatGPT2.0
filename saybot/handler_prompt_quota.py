5dd8e54e03c6e054048a2b93aa532efa5af983cd
BIBIN KUNJUMON <bibinkunjumon2020@gmail.com>
2023-05-03 17:56:24 +0530

    Prompt Quota Update Issue Fixed with another variable

--- saybot/handler_prompt_quota.py
+++ saybot/handler_prompt_quota.py
@@ -4,7 +4,7 @@ db_path = os.path.join(os.getcwd(),"database","user_data.db")
 
 async def check_prompt_balance(update:Update): # daily prompt limit is defined and managed
     user_id = update.message.from_user.id # individual user IDs
-    prompt_quota=100 # manually fixing it
+    prompt_quota_standard=100 # It is the original standard quota for all Users
     
     with sqlite3.connect(db_path) as connection:
         cursor = connection.cursor()
@@ -13,17 +13,17 @@ async def check_prompt_balance(update:Update): # daily prompt limit is defined a
       
         try:
             if data is not None :
-                prompt_balance = data[2]  
-                prompt_quota = data[3]
+                prompt_balance = data[2]  # Value from already existing data in DB
+                prompt_quota = data[3]  
                 #time data need to convert from string in DB to datetime format.
                 last_accessed_time = datetime.strptime(data[1],'%Y-%m-%d %H:%M:%S.%f')
                 now = datetime.now() 
-                if(now - last_accessed_time) < timedelta(hours=24) and prompt_balance <= 0 :# checking 24 hr and prompt balance
-                    logging.info("Prompt Quota Finished Promt Balance = "+str(prompt_balance))
+                if(now - last_accessed_time) < timedelta(hours=24) and prompt_balance <= 0 :# checking 24 hr and prompt balance below zero 
+                    logging.info("Prompt Quota Finished.\nYour Promt Balance = "+str(prompt_balance))
                     return False
                 elif(now-last_accessed_time) >= timedelta(hours=24):      # If 24 hr crossed from last usage:Recharge Balance
-                    prompt_balance=prompt_quota  # recharged the prompt balance
-                    await store_prompt_quota(update=update,prompt_balance = prompt_balance,prompt_quota=prompt_quota)
+                    prompt_balance=prompt_quota_standard  # recharged the prompt balance
+                    await store_prompt_quota(update=update,prompt_balance = prompt_balance,prompt_quota=prompt_quota_standard)
                     logging.info(" Recharged Prompt Balance = "+str(prompt_balance))
                     return True
                 else:
@@ -32,8 +32,8 @@ async def check_prompt_balance(update:Update): # daily prompt limit is defined a
                     return True
 
             else:#data is none - User is for the first time
-                prompt_balance = prompt_quota - 1 # reducing the prompt balance by '1'
-                await store_prompt_quota(update=update,prompt_balance = prompt_balance,prompt_quota=prompt_quota)
+                prompt_balance = prompt_quota_standard - 1 # reducing the prompt balance by '1'
+                await store_prompt_quota(update=update,prompt_balance = prompt_balance,prompt_quota=prompt_quota_standard)
                 return True
         except Exception as e:
             logging.error(e)
--- saybot/main.py
+++ saybot/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     application = Application.builder().token(token=os.getenv("API_BOT")).build()
     
     application.add_handler(CommandHandler('start',handle_start_command))
-    application.add_handler(CommandHandler('gpt35turbo',handle_command_gpt35turbo))
+    application.add_handler(CommandHandler('gpt35turbo',handle_command_gpt35turbo)) # Model gpt-3.5-turbo used
     application.add_handler(CommandHandler('textdavinci003',handle_command_textdavinci003))
     # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,handle_message))
     application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND,handle_message))
